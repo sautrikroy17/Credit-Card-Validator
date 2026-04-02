452fe7518974733a0bcc51f340b7ff53e820511a
Sautrik <sr9973@srmist.edu.in>
2026-04-03 00:19:46 +0530

    modified file with luhn logic

--- credit.py
+++ credit.py
@@ -9,16 +9,36 @@ def get_card_network(card_number):
         return "Discover"
     return "Unknown"
 
+def validate_luhn(card_number):
+    total = 0
+    reverse_digits = card_number[::-1]
+    
+    for i, char in enumerate(reverse_digits):
+        digit = int(char)
+        
+        if i % 2 == 1:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+                
+        total += digit
+        
+    return total % 10 == 0
+
 def main():
     raw_input = input("Enter credit card number: ")
+    
     card_number = raw_input.replace(" ", "").replace("-", "")
     
     if not card_number.isdigit() or len(card_number) < 13 or len(card_number) > 19:
         print("INVALID - Check length and characters")
         return
         
-    network = get_card_network(card_number)
-    print(f"Format check passed. Detected network: {network}")
+    if validate_luhn(card_number):
+        network = get_card_network(card_number)
+        print(f"VALID - Detected network: {network}")
+    else:
+        print("INVALID - Failed Luhn check")
 
-if __name__ == " __main__ ":
+if __name__ == "__main__":
     main()
